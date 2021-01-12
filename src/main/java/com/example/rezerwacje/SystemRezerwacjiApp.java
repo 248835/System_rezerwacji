c3e1c75ad93fbdd0c5b97aea534bc7f33c464cf6
248835 <248835@student.pwr.edu.pl>
2021-01-12 20:26:26 +0100

    mały update dla Heroku

---
+++ src/main/java/com/example/rezerwacje/SystemRezerwacjiApp.java
@@ -0,0 +1,11 @@
+package com.example.rezerwacje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemRezerwacjiApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SystemRezerwacjiApp.class,args);
+    }
+}
