3c28fac939c0b5070f8626aefe93ab9debc0bc23
248835 <248835@student.pwr.edu.pl>
2021-01-12 21:04:19 +0100

    heroku plz work

--- src/main/java/com/example/rezerwacje/conf/RootConfig.java
+++ src/main/java/com/example/rezerwacje/conf/RootConfig.java
@@ -8,14 +8,14 @@ import org.springframework.core.type.filter.RegexPatternTypeFilter;
 import java.util.regex.Pattern;
 
 @Configuration
-@ComponentScan(basePackages = {"com.example.rezerwacje"},
+@ComponentScan(basePackages = {"com.example.rezerwacje"}/*,
         excludeFilters = {
                 @ComponentScan.Filter(type = FilterType.CUSTOM, value= RootConfig.WebPackage.class)
-        })
+        }*/)
 public class RootConfig {
-    public static class WebPackage extends RegexPatternTypeFilter {
-        public WebPackage() {
-            super(Pattern.compile("com\\.example\\.rezerwacje\\.web"));
-        }
-    }
+//    public static class WebPackage extends RegexPatternTypeFilter {
+//        public WebPackage() {
+//            super(Pattern.compile("com\\.example\\.rezerwacje\\.web"));
+//        }
+//    }
 }
